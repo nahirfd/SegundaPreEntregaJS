9668308714ea6bcbb00700f24183f6c7be376a27
Nahir Ferreyra <nahirfdubouloy@gmail.com>
2023-11-20 11:39:44 -0300

    archivo JSON, promesas y asincronias

--- js/index.js
+++ js/index.js
@@ -6,48 +6,65 @@ const modalContainer = document.getElementById("modal-container");
 
 const cantidadCarrito = document.getElementById("cantidadCarrito");
 
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-productos.forEach((product) => {
-  let content = document.createElement("div");
-  content.className = "card"
-  content.innerHTML = `
-  <img src="${product.img}">
-  <h3>${product.nombre}</h3>
-  <p class="price"> Precio: $${product.precio}</p>
-  `;
+const getProducts = async () => {
+  const response = await fetch("data.json");
+  const data = await response.json();
 
-  shopContent.append(content);
 
-  let comprar = document.createElement("button")
-  comprar.innerText = "Agregar al carrito"
-  comprar.className = "comprar"
-  content.append(comprar)
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card"
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="price"> Precio: $${product.precio}</p>
+    `;
 
-  comprar.addEventListener("click", () => {
+    shopContent.append(content);
 
-    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+    let comprar = document.createElement("button")
+    comprar.innerText = "Agregar al carrito"
+    comprar.className = "comprar"
+    content.append(comprar)
 
-    if (repeat) {
-      carrito.map((prod) => {
-        if (prod.id === product.id) {
-          prod.cantidad++;
-        }
-      })
-    } else {
-      carrito.push({
-        id: product.id,
-        img: product.img,
-        nombre: product.nombre,
-        precio: product.precio,
-        cantidad: product.cantidad,
-      })
-    }
-    console.log(carrito)
-    carritoCounter();
-  })
-});
+    comprar.addEventListener("click", () => {
+
+      const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+
+      if (repeat) {
+        carrito.map((prod) => {
+          if (prod.id === product.id) {
+            prod.cantidad++;
+          }
+        })
+      } else {
+        carrito.push({
+          id: product.id,
+          img: product.img,
+          nombre: product.nombre,
+          precio: product.precio,
+          cantidad: product.cantidad,
+        })
+      }
+      console.log(carrito)
+      carritoCounter();
+      saveLocal();
+    })
+  });
+};
+
+getProducts();
+
+
+//set item
 
 function saveLocal() {
   localStorage.setItem("carrito", JSON.stringify(carrito));
-}
+};
+
+
+
+
+
